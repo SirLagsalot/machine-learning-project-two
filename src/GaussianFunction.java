e2bbb5d90412296885bfbf69d263cbe329fbc50f
Pookey <pookey@Pookeys-MacBook-Pro.local>
2017-10-06 15:57:14 -0600

    commenting in the functions

--- src/GaussianFunction.java
+++ src/GaussianFunction.java
@@ -2,13 +2,13 @@ public class GaussianFunction implements IActivationFunction {
 
     @Override
     public double compute(double value) {
-        //tanh
+        // 1/(sqrt(2*pi*sigma) * e^(-(current - mean)^2/(2*sigma^2))
         return 0;
     }
 
     @Override
     public double computeDerivative(double value) {
-        // 4/(e^x + e^-x)^2
+        //((x - mu)*e^(-((x - mu)^2)/(2*sigma^2)))/(sqrt(2*pi)*sigma^(5/2))
         return 0;
     }
 }
