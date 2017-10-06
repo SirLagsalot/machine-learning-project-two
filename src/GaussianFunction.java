61cecb5cab4649c7f1fdf77374136730906ccc8a
Pookey <pookey@Pookeys-MacBook-Pro.local>
2017-10-06 11:35:25 -0600

    commenting Karen's stuff

--- src/GaussianFunction.java
+++ src/GaussianFunction.java
@@ -2,11 +2,13 @@ public class GaussianFunction implements IActivationFunction {
 
     @Override
     public double compute(double value) {
+        //tanh
         return 0;
     }
 
     @Override
     public double computeDerivative(double value) {
+        // 4/(e^x + e^-x)^2
         return 0;
     }
 }
