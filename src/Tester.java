af4bdb5a303cecff32a7bbba79bb4f639ef9453f
Pookey <pookey@Pookeys-MacBook-Pro.local>
2017-10-07 14:57:13 -0600

    karen sucks at syntax. setting Gaussian function

---
+++ src/Tester.java
@@ -0,0 +1,13 @@
+public class Tester {
+
+    public void crossValidation(Sample[] dataset) {
+        // Do five times
+            // Shuffle data
+            // Split data in half, training/test
+            // Train FFN, RBF on training set
+            // Test FFN, RBF on test set
+            // Tally stats
+            // Swap sets, repeat
+        // Print the stuffs
+    }
+}
