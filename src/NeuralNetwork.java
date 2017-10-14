05fadfc9af2ba36ec7bd15e8ae5a6693c0b6e052
Zach Connelly <zmconnelly@gmail.com>
2017-10-14 17:50:04 -0600

    gradient function

--- src/NeuralNetwork.java
+++ src/NeuralNetwork.java
@@ -2,10 +2,12 @@ import java.util.List;
 
 public abstract class NeuralNetwork implements IFunctionApproximator {
 
+    protected IActivationFunction activationFunction;
+
     protected int numInputs;
     protected int numOutputs;
 
-    public NeuralNetwork(int numInputs, int numOutputs) {
+    public NeuralNetwork(int numInputs, int numOutputs, IActivationFunction activationFunction) {
         this.numInputs = numInputs;
         this.numOutputs = numOutputs;
     }
@@ -13,4 +15,12 @@ public abstract class NeuralNetwork implements IFunctionApproximator {
     public abstract void train(List<Sample> samples);
 
     public abstract double[] approximate(double[] inputs);
+
+    protected double[] computeOutputErrorGradient(double[] networkOutputs, double[] expectedOutputs) {
+        double[] error = new double[networkOutputs.length];
+        for (int i = 0; i < networkOutputs.length; i++) {
+            error[i] = (networkOutputs[i] - expectedOutputs[i]) * this.activationFunction.computeDerivative(networkOutputs[i]);
+        }
+        return error;
+    }
 }
